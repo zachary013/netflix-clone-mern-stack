b253e8ba9c076c6d9706bd228ba4a0f1bd2eaf18
Zakariae Azn <azarkanzakariae@gmail.com>
2024-08-18 22:05:04 +0100

    Movies section

---
+++ backend/controllers/movie.controller.js
@@ -0,0 +1,80 @@
+
+import { fetchFromTMDB } from "../services/tmdb.service.js";
+
+//Get trending movies and display them in a dynamic way
+export async function getTrendingMovie(req, res) {
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`);
+        
+        if (!data.results || data.results.length === 0) {
+            return res.status(404).json({ success: false, message: "No trending movies found" });
+        }
+
+        const randomMovie = data.results[Math.floor(Math.random() * data.results.length)];
+
+        res.json({ success: true, content: randomMovie });
+    } catch (error) {
+        console.log("Error in getTrendingMovie controller", error.message);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
+//Get movie trailers and display them in a dynamic way
+export async function getMovieTrailers(req, res) {
+	const { id } = req.params;
+	try {
+		const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`);
+		res.json({ success: true, trailers: data.results });
+	} catch (error) {
+		if (error.message.includes("404")) {
+			return res.status(404).send(null);
+		}
+
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+}
+
+//Get movie details and display them in a dynamic way
+export async function getMovieDetails(req, res) {
+    const { id } = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}?language=en-US`);
+        res.status(200).json({ success: true, details: data });
+    } catch (error) {
+        if (error.message.includes("404")) {
+            return res.status(404).send(null);
+        }
+
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+//Get similar movies and display them in a dynamic way
+export async function getSimilarMovies(req, res) {
+    const { id } = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${id}/similar?language=en-US&page=1`);
+        res.json({ success: true, similarMovies: data.results });
+    } catch (error) {
+        if (error.message.includes("404")) {
+            return res.status(404).send(null);
+        }
+
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
+
+//Get movies by category and display them in a dynamic way
+export async function getMovieByCategory(req, res) {
+    const { category } = req.params;
+    try {
+        const data = await fetchFromTMDB(`https://api.themoviedb.org/3/movie/${category}?language=en-US&page=1`);
+        res.json({ success: true, movies: data.results });
+    } catch (error) {
+        if (error.message.includes("404")) {
+            return res.status(404).send(null);
+        }
+
+        res.status(500).json({ success: false, message: "Internal Server Error" });
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import authRoutes from './routes/auth.route.js';
+import movieRoutes from './routes/movie.route.js';
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
@@ -11,9 +12,12 @@ const PORT = ENV_VARS.PORT
 
 app.use(express.json());  // to use req.body in auth.controller.js signup
 
+//routes
 app.use("/api/auth", authRoutes);
+app.use("/api/movie", movieRoutes);
 
 app.listen(PORT, () => {
     console.log("Server started at http://localhost:" + PORT);
     connectDB();
 });
+
